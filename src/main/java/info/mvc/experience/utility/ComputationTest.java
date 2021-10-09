416e1dde70f9339f9b7bff25f07dc62213010410
kaung <kaunghtetmyint@live.co.uk>
2021-10-09 16:49:40 +0630

    computation commit init

--- src/main/java/info/mvc/experience/utility/ComputationTest.java
+++ src/main/java/info/mvc/experience/utility/ComputationTest.java
@@ -1,5 +1,6 @@
 package info.mvc.experience.utility;
 
+import info.mvc.experience.model.CompareResult;
 import info.mvc.experience.model.CsvRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,43 +40,147 @@ public class ComputationTest {
 //            e.printStackTrace();
 //        }
 
+        final List<CsvRecord> fileOneCsvRecordList = convertToCsvRecordList(LOAD_CSV_FILENAME_ONE);
+        final List<CsvRecord> fileTwoCsvRecordList = convertToCsvRecordList(LOAD_CSV_FILENAME_TWO);
 
-        final Map<String, List<CsvRecord>> fileOneRecordMap = getIntoRecordMap(LOAD_CSV_FILENAME_ONE);
-        final Map<String, List<CsvRecord>> fileTwoRecordMap = getIntoRecordMap(LOAD_CSV_FILENAME_TWO);
-        System.out.println("==================================Print Out Record Map=============");
-        int matchCountAgainstFileTwo =0;
-        int unmatchCountAgainstFileTwo =0;
-        List<String> unmatchKeyListAgainstFileTwo = new ArrayList<>();
+        final Map<String, List<CsvRecord>> fileOneRecordMap = getIntoRecordMap(fileOneCsvRecordList);
+        final Map<String, List<CsvRecord>> fileTwoRecordMap = getIntoRecordMap(fileTwoCsvRecordList);
+        System.out.println("==================================File One - Checking Record Map=============");
+        int fileOneTotalRecordCount = 0;
+        int fileTwoTotalRecordCount = 0;
 
-        fileOneRecordMap.forEach((key,listOfCsvRecord)->{
-            logger.info("{}={}, CsvRecord Per Key={}",COMPARE_KEY, key, listOfCsvRecord.size());
+        int matchCountAgainstFileTwo = 0;
+        int notFoundCountAgainstFileTwo = 0;
+        List<String> notFoundKeyListAgainstFileTwo = new ArrayList<>();
 
-        });
-        System.out.println("==================================Print Out Record Map=============");
-        fileTwoRecordMap.forEach((key,listOfCsvRecord)->{
-            logger.info("{}={}, CsvRecord Per Key={}",COMPARE_KEY, key, listOfCsvRecord.size());
-        });
-        System.out.println("===================================================================");
+        int foundButNotMatch_Count_AgainstFileTwo = 0;
+        List<String> foundButNotMatch_KeyList_AgainstFileTwo = new ArrayList<>();
 
+        fileOneTotalRecordCount = fileOneCsvRecordList.size()-1;
+
+        fileTwoTotalRecordCount = fileTwoCsvRecordList.size()-1;
+
+//        fileOneRecordMap.forEach((key,listOfCsvRecord)->{
+//            logger.info("{}={}, CsvRecord Per Key={}",COMPARE_KEY, key, listOfCsvRecord.size());
+//
+//        });
         for (String key : fileOneRecordMap.keySet()) {
-            boolean found = fileTwoRecordMap.containsKey(key)?true:false;
+
             if(fileTwoRecordMap.containsKey(key)){
-                matchCountAgainstFileTwo++;
+                //Found ;;; Record with Same Key Exists in File Two ;;; further Matching
+                List<CsvRecord> csvRecordsFileOne = fileOneRecordMap.get(key);
+                if(csvRecordsFileOne.size()>1){
+                    // handle duplicate / having more than 1 Csv Records per key ID
+                    logger.warn("Having more than 1 Csv Records per key ID={}", key);
+                    for (CsvRecord csvRecordOne : csvRecordsFileOne) {
+                        List<CsvRecord> csvRecordsFileTwo = fileTwoRecordMap.get(key);
+                        for (CsvRecord csvRecordTwo : csvRecordsFileTwo) {
+                            CompareResult compareResult =  compareTwoCsvRecords(csvRecordOne, csvRecordTwo);
+
+                            logger.info("Having Same Key={}, Result ={}", key, compareResult);
+                        }
+                        //matchCountAgainstFileTwo++;
+                    }
+
+
+                } else if (csvRecordsFileOne.size()==1){
+                    CsvRecord csvRecordOne = csvRecordsFileOne.get(0);
+                    List<CsvRecord> csvRecordsFileTwo = fileTwoRecordMap.get(key);
+
+                    List<CompareResult> compareResultList = new ArrayList<>();
+                    for (CsvRecord csvRecordTwo : csvRecordsFileTwo) {
+                        CompareResult compareResult =  compareTwoCsvRecords(csvRecordOne, csvRecordTwo);
+                        compareResultList.add(compareResult);
+                        if( !compareResult.isRecordMatch() ) {
+                            logger.info("Key={}, Unmatch Result ={}", key, compareResult);
+                            foundButNotMatch_Count_AgainstFileTwo++;
+                            foundButNotMatch_KeyList_AgainstFileTwo.add(key);
+                        } else {
+                            matchCountAgainstFileTwo++;
+                        }
+
+                    }
+                    //logger.info("Key={}, Number of Comparing RecordTwo={}", key, compareResultList.size());
+
+
+
+
+                } else {
+                    //do nothing ??
+                    logger.warn("DO Nothing");
+                }
+
             } else{
-                unmatchKeyListAgainstFileTwo.add(key);
-                unmatchCountAgainstFileTwo++;
+                notFoundKeyListAgainstFileTwo.add(key);
+                notFoundCountAgainstFileTwo++;
             }
         }
-        logger.info("\nmatchCountAgainstFileTwo={} \nunmatchCountAgainstFileTwo={}",
-                matchCountAgainstFileTwo, unmatchCountAgainstFileTwo);
-        logger.info("\nunmatchKeyListAgainstFileTwo={}",unmatchKeyListAgainstFileTwo.toString());
+        System.out.println("==================================File Two - Checking Record Map=============");
+//        fileTwoRecordMap.forEach((key,listOfCsvRecord)->{
+//            logger.info("{}={}, CsvRecord Per Key={}",COMPARE_KEY, key, listOfCsvRecord.size());
+//        });
+        System.out.println("===================================================================");
+
+
+        logger.info("=====File One======\nfileOneTotalRecordCount={} " +
+                        "\nmatchCountAgainstFileTwo={} " +
+                        "\nnotFoundCountAgainstFileTwo={} " +
+                        "\nfoundButNotMatch_Count_AgainstFileTwo={}",
+                fileOneTotalRecordCount,
+                matchCountAgainstFileTwo,
+                notFoundCountAgainstFileTwo,
+                foundButNotMatch_Count_AgainstFileTwo);
+
+        logger.info("\nfoundButNotMatch_KeyList_AgainstFileTwo={}", foundButNotMatch_KeyList_AgainstFileTwo.toString());
+        logger.info("\nnotFoundKeyListAgainstFileTwo={}",notFoundKeyListAgainstFileTwo.toString());
         //fileOneRecordMap.entrySet();
 
 
     }
 
-    static private Map<String, List<CsvRecord>> getIntoRecordMap(String localFilePath) throws Exception {
+    static private CompareResult compareTwoCsvRecords(CsvRecord one, CsvRecord two){
 
+
+        CompareResult result = new CompareResult();
+
+        result.setRecordMatch(true);
+        for (String headerKey : one.getTransaction().keySet()) {
+
+            String twoValue = two.getTransaction().get(headerKey);
+            String oneValue = one.getTransaction().get(headerKey);
+            List<String> headers = new ArrayList<>();
+            List<String> recordOneValues = new ArrayList<>();
+            List<String> recordTwoValues = new ArrayList<>();
+
+            if(!twoValue.equalsIgnoreCase(oneValue) ){
+
+                result.setRecordMatch(false);
+
+                result.setReason("Record Values Not Matched");
+
+                headers.add(headerKey);
+                result.setComparedHeader(headers);
+
+                recordOneValues.add(oneValue);
+                result.setValueInFileOne(recordOneValues);
+
+                recordTwoValues.add(twoValue);
+                result.setValueInFileTwo(recordTwoValues);
+
+
+                result.setRowNumberInFileOne(String.valueOf(one.getRowNumber()));
+                result.setRowNumberInFileTwo(String.valueOf(two.getRowNumber()));
+
+            }
+
+        }
+//        if (resultsMap.containsValue(false)) return resultsMap;
+//
+//        resultsMap.put("AllMatch", String.valueOf(true));
+        return result;
+    }
+
+    static private List<CsvRecord> convertToCsvRecordList(String localFilePath) throws Exception {
         //Testing with Local file
         System.out.println("Receiving request to Load and Convert Filename="+localFilePath);
         File file = Paths.get(localFilePath).toFile();
@@ -85,10 +190,10 @@ public class ComputationTest {
 
         Map<String, Integer> headerMap = new LinkedHashMap<>();
         List<CsvRecord> csvRecordList = new ArrayList<>();
-        int rowIndex = 0;
+        int rowIndex = 1;
         for (String[] arrays : result) {
 
-            if (rowIndex == 0) { //READING for header only
+            if (rowIndex == 1) { //READING for header only ;;; HEADER row starts at Row 1
                 for (int i = 0; i < arrays.length; i++) {
                     headerMap.put(arrays[i], i);
                 }
@@ -107,8 +212,15 @@ public class ComputationTest {
 
         }
         System.out.println("===============================");
-        System.out.println("Total Row/Records (-Headers)="+(csvRecordList.size()-1));
+        System.out.println("Total Records (-Headers)="+(csvRecordList.size()-1));
         System.out.println("===============================");
+
+        return csvRecordList;
+    }
+
+    static private Map<String, List<CsvRecord>> getIntoRecordMap(List<CsvRecord> csvRecordList) throws Exception {
+
+
 //        for (CsvRecord csvRecord : csvRecordList) {
 //            csvRecord.getDataByHeader();
 //            logger.info("Row:{}", csvRecord.getRowNumber());
@@ -118,7 +230,7 @@ public class ComputationTest {
 
         Map<String, List<CsvRecord>> csvRecordMap = csvRecordList
                 .stream()
-                .filter(csvRecord -> csvRecord.getRowNumber() != 0)
+                .filter(csvRecord -> csvRecord.getRowNumber() != 1)
 
                 .collect(Collectors.groupingBy((map) ->
                         map.getTransaction().get(COMPARE_KEY), LinkedHashMap::new, Collectors.toList()));
